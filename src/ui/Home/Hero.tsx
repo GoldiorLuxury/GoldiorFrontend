edc913dde430f177d3eccf77495effbc3985f323
Shreya Sapre <shreyasapre4@gmail.com>
2024-11-09 20:38:19 +0530

    Home page refining

--- src/ui/Home/Hero.tsx
+++ src/ui/Home/Hero.tsx
@@ -11,11 +11,11 @@ function Hero({
    
     return (
       <div className="bg-white flex flex-col-reverse items-center justify-between w-full p-6 -mt-6 lg:flex-row lg:px-28 lg:p-0 lg:mt-0">
-        <div className="flex flex-col justify-center items-center text-center p-0 m-0 lg:items-start">
-          <h1 className="text-2xl mt-1 lg:text-5xl lg:my-1.5">Elevate your spirit with</h1>
-          <h1 className="text-2xl mt-1 lg:text-5xl lg:my-1.5">Victory Scented Fragrances!</h1>
-          <p className=" lg:text-xl text-brown-600 mb-6 lg:mt-2">{subtext}</p>
-          <button className="bg-[var(--theme-brown)] text-white px-6 py-2 rounded-lg w-1/2 lg:mt-1 lg:w-1/3">
+        <div className="flex flex-col justify-center items-center text-center p-0 m-0 lg:-mt-20 lg:items-start">
+          <p className="text-2xl mt-1 lg:text-6xl lg:my-1.5 ">Elevate your spirit with</p>
+          <p className="text-2xl mt-1 lg:text-6xl lg:my-1.5 ">Victory Scented Fragrances!</p>
+          <p className=" lg:text-2xl text-brown-600 mb-4 lg:mb-16 lg:mt-2">{subtext}</p>
+          <button className="bg-[var(--theme-brown)] text-white px-6 py-3 rounded-lg w-1/2 lg:mt-1 lg:w-1/5">
             {buttonText}
           </button>
         </div>
